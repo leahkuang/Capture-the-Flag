06b0232b82c4d62f046441a129f2b14742e99767
leahkuang <125530563+leahkuang@users.noreply.github.com>
2025-07-24 23:56:32 +0800

    Create redis.py

---
+++ server/services/redis.py
@@ -0,0 +1,25 @@
+import json
+import asyncio
+from redis import asyncio as aioredis  
+
+redis = None
+pub = None
+sub = None
+
+async def init_redis():
+    global redis, pub, sub
+    redis = aioredis.Redis(host="redis", port=6379, decode_responses=True)
+    pub = redis
+    sub = redis.pubsub()
+    await sub.subscribe("game")
+    return redis
+
+async def publish(channel, msg):
+    await pub.publish(channel, json.dumps(msg))
+
+async def subscribe():
+    while True:
+        msg = await sub.get_message(ignore_subscribe_messages=True, timeout=1.0)
+        if msg and 'data' in msg:
+            yield json.loads(msg["data"])
+        await asyncio.sleep(0.01)
